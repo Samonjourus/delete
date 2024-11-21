e2fc2e84f7add20f2786e3fbf182cb7687ce8726
benjamin standfield <benjaminstandfield@gmail.com>
2024-11-21 09:47:28 -0500

    doc: added documentation for add

--- main.py
+++ main.py
@@ -1,4 +1,13 @@
 def add(num1, num2):
+    """adds two number
+
+    Args:
+        num1 (float): first summand
+        num2 (float): second summand
+
+    Returns:
+        float: the sum of the two numbers
+    """
     return num1 + num2
     
 def test_should_add_basic_number():
